7496c51802f26dc06a831ce225eb8aecd423ffda
Jess Wilson-Woodrow <jess@techelevator.com>
2022-05-25 21:50:05 +0000

    Update dropdb.sql

--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -5,9 +5,9 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'final_capstone';
+WHERE datname = 'te_gram';
 
-DROP DATABASE final_capstone;
+DROP DATABASE te_gram;
 
-DROP USER final_capstone_owner;
-DROP USER final_capstone_appuser;
+DROP USER te_gram_owner;
+DROP USER te_gram_appuser;
