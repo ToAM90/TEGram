dbe09934ec62cd4e2ad4fb4fc014a774849814a8
Jess Wilson-Woodrow <jess@techelevator.com>
2022-05-25 21:50:21 +0000

    Update user.sql

--- java/database/user.sql
+++ java/database/user.sql
@@ -2,24 +2,24 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER final_capstone_owner
+CREATE USER te_gram_owner
 WITH PASSWORD 'finalcapstone';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_owner;
+TO te_gram_owner;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_owner;
+TO te_gram_owner;
 
-CREATE USER final_capstone_appuser
+CREATE USER te_gram_appuser
 WITH PASSWORD 'finalcapstone';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_appuser;
+TO te_gram_appuser;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_appuser;
+TO te_gram_appuser;
