3aa0d762dcfda7b52baf03c132e0547bca0d8c09
Spencer Cindia <Spencer3033@gmail.com>
2022-06-02 15:37:16 -0400

    passing over to kameron

--- vue/src/components/ImageColumn.vue
+++ vue/src/components/ImageColumn.vue
@@ -1,23 +1,47 @@
 <template>
-  <div class="my-images" > 
-    <div class='feed-post-container' v-for="image in images" v-bind:key="image.id">
-      <img class="image"  v-bind:src="image" alt="" />
-      
-      <div class='post-interaction-bar'>
-        <img class='like-icon interaction-icon' @click='like' v-if='liked' src="@/resources/icons8-heart-50 (outline).png" alt="">
-        
-        <img class='like-icon interaction-icon' @click='like' v-else src="@/resources/icons8-heart-50 (outline).png" alt="">
-        <p>view more</p>
-        <img class='favorite-icon interaction-icon' @click='like' src="@/resources/icons8-star-50 (outline).png" alt="">
+  <div class="my-images">
+    <div
+      class="feed-post-container"
+      v-for="image in images"
+      v-bind:key="image.id"
+    >
+      <img class="image" v-bind:src="image" alt="" />
+
+      <div class="post-interaction-bar">
+        <img
+          class="like-icon interaction-icon"
+          @click="like"
+          v-if="liked"
+          src="@/resources/icons8-heart-50 (outline).png"
+          alt=""
+        />
+
+        <img
+          class="like-icon interaction-icon"
+          @click="like"
+          v-else
+          src="@/resources/icons8-heart-50 (outline).png"
+          alt=""
+        />
+        <img
+          class="view-more interaction-icon"
+          src="@/resources/expand_more_FILL0_wght400_GRAD0_opsz48.png"
+          alt=""
+        />
+        <img
+          class="favorite-icon interaction-icon"
+          @click="like"
+          src="@/resources/icons8-star-50 (outline).png"
+          alt=""
+        />
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
 export default {
-  name: 'image-column',
+  name: "image-column",
   computed: {
     images() {
       return this.$store.state.images;
@@ -25,103 +49,94 @@ export default {
   },
   methods: {
     like() {
-      this.image.id
-    }
-  }
+      this.image.id;
+    },
+  },
 };
 </script>
 
 <style>
-@media only screen and (max-width: 800px)
-  {
-    .my-images {
-      margin-left: 295px;
-      line-height: 0;
-      height: 100%;
-      
-    
-      background: var(--primary-background-color);
-    }
-  }
-
-@media only screen and (max-width: 1400px)
-  {
-    .my-images {
-      margin-left: 295px;
-      line-height: 0;
-      height: 100%;
-      
-      -webkit-column-count: 2;
-      -webkit-column-gap:   0px;
-      -moz-column-count:    2;
-      -moz-column-gap:      0px;
-      column-count:         2;
-      column-gap:           0px;  
-      background: var(--primary-background-color);
-    }
-  }
-  @media only screen and (min-width: 1400px)
-  {
+@media only screen and (max-width: 800px) {
   .my-images {
     margin-left: 295px;
     line-height: 0;
     height: 100%;
-    
-    -webkit-column-count: 3;
-    -webkit-column-gap:   0px;
-    -moz-column-count:    3;
-    -moz-column-gap:      0px;
-    column-count:         3;
-    column-gap:           0px;  
+
     background: var(--primary-background-color);
   }
+}
+
+@media only screen and (max-width: 1400px) {
+  .my-images {
+    margin-left: 295px;
+    line-height: 0;
+    height: 100%;
+
+    -webkit-column-count: 2;
+    -webkit-column-gap: 0px;
+    -moz-column-count: 2;
+    -moz-column-gap: 0px;
+    column-count: 2;
+    column-gap: 0px;
+    background: var(--primary-background-color);
   }
+}
+@media only screen and (min-width: 1400px) {
+  .my-images {
+    margin-left: 295px;
+    line-height: 0;
+    height: 100%;
 
-  
-
-  
-
-  .my-images .feed-post-container {
-    margin-left: 0px;
-    margin-right: 0px;
-    width: calc(100% - 40px)!important;
-    height: auto !important;
-    box-shadow: 0px 0px 5px rgb(172, 169, 169);
-    border-radius: 5%;
-    
-    /* margin-top: 5px; */
-    border-top: 0px
+    -webkit-column-count: 3;
+    -webkit-column-gap: 0px;
+    -moz-column-count: 3;
+    -moz-column-gap: 0px;
+    column-count: 3;
+    column-gap: 0px;
+    background: var(--primary-background-color);
   }
+}
 
-  .my-images .feed-post-container > .image{
-    width: calc(100%)!important;
-  }
+.my-images .feed-post-container {
+  margin-left: 0px;
+  margin-right: 0px;
+  width: calc(100% - 40px) !important;
+  height: auto !important;
+  box-shadow: 0px 0px 5px rgb(172, 169, 169);
+  border-radius: 5%;
 
+  /* margin-top: 5px; */
+  border-top: 0px;
+}
 
-  .post-interaction-bar {
-    background: white;
+.my-images .feed-post-container > .image {
+  width: calc(100%) !important;
+}
 
-    box-shadow: 0px 0px 5px rgb(172, 169, 169);
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    height: 20px;
-    margin-bottom: 5px;
-    color: black;
-  }
+.post-interaction-bar {
+  background: white;
 
-  .interaction-icon {
-    max-height: auto;
-    max-width: 20px
-  }
+  box-shadow: 0px 0px 5px rgb(172, 169, 169);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 20px;
+  margin-bottom: 5px;
+  color: black;
+}
 
-  .like-icon {
-  }
- 
-  .image-info {
-    border-left: solid black 3px;
-    /* max-width: 20%; */
-    color: black;
-    padding-left: 5px
-  }
+.interaction-icon {
+  max-height: auto;
+  max-width: 20px;
+}
+
+.like-icon {
+}
+
+.image-info {
+  border-left: solid black 3px;
+  /* max-width: 20%; */
+  color: black;
+  padding-left: 5px;
+}
 </style>
--- vue/src/views/Upload.vue
+++ vue/src/views/Upload.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="upload">
+    <button id="image-input" v-on:click="uploadPhoto">Upload Image</button>
+    <form id="upload-form" v-on:submit.prevent="uploadPost">
+      <img id="image-preview" v-bind:src="imageUrl" />
 
-      
-       <button id="image-input" v-on:click="uploadPhoto">Upload Image</button>
-      <form id="upload-form" v-on:submit.prevent="uploadPost">
-        
-        <img id=image-preview v-bind:src='imageUrl'/>
-
-          <input id="caption"
-          type="text"
-          placeholder="Enter caption"
-          v-model="post.caption"/>
-        <div id=privated>
-          <input id="checkbox" type="checkbox" v-model="post.privated" />
-          <label for="checkbox">Private {{privated}}</label> <br>
-        </div>
-        <button id="submit" type="submit">Post</button>
-      </form>
-      
+      <input
+        id="caption"
+        type="text"
+        placeholder="Enter caption"
+        v-model="post.caption"
+      />
+      <div id="privated">
+        <input id="checkbox" type="checkbox" v-model="post.privated" />
+        <label for="checkbox">Private {{ privated }}</label> <br />
+      </div>
+      <button id="submit" type="submit">Post</button>
+    </form>
   </div>
 </template>
 
@@ -25,72 +23,68 @@
 import postService from "../services/PostService";
 
 export default {
-    name: "upload",
-    data() {
-        return {
-            post: {
-                caption: "",
-                img: "",
-                privated: false
-            },
-            imageUrl: "",
-            preview: true
-        }
-        
+  name: "upload",
+  data() {
+    return {
+      post: {
+        caption: "",
+        img: "",
+        privated: false,
+      },
+      imageUrl: "",
+      preview: true,
+    };
+  },
+  methods: {
+    uploadPost() {
+      postService.addPost(this.post);
+      this.imageUrl = "";
+      this.post.caption = "";
+      this.post.img = "";
+      this.privated = false;
     },
-    methods: {
-        uploadPost(){
-            postService.addPost(this.post)
-            this.imageUrl = ""
-            this.post.caption = ""
-            this.post.img = ""
-            this.privated = false
-        },
 
-        uploadPhoto(){
-             window.cloudinary
+    uploadPhoto() {
+      window.cloudinary
         .openUploadWidget(
           { cloud_name: "dcipg5scy", upload_preset: "TE-GRAM", maxFiles: 1 },
           (error, result) => {
             if (!error && result && result.event === "success") {
-                console.log(result.info.url)
-                this.post.img = result.info.url
-                this.imageUrl = result.info.url
-
+              console.log(result.info.url);
+              this.post.img = result.info.url;
+              this.imageUrl = result.info.url;
             } else {
-                console.log(error)
-            }}
+              console.log(error);
+            }
+          }
         )
         .open();
-
-        }
     },
-
-}
+  },
+};
 </script>
 
 <style>
-
-#upload{
-    text-align: center;
+#upload {
+  text-align: center;
 }
 
-#image-input{
-    display: block;
-    margin: auto;
+#image-input {
+  display: block;
+  margin: auto;
 }
 
-#caption{
-    display: block;
-    width: 20rem;
-    margin: auto;
+#caption {
+  display: block;
+  width: 20rem;
+  margin: auto;
 }
 
-#privated{
-    display: block;
+#privated {
+  display: block;
 }
 
-img{
-    width: 400px;
+#image-preview {
+  width: 400px;
 }
 </style>
