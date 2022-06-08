60b5666fea70ef9f5b50cd8acc7470b993534e18
Danny Nguyen <danny3698@gmail.com>
2022-06-08 14:11:28 -0700

    added backend functionality of liked list

--- java/src/main/java/com/techelevator/dao/JdbcLikedDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcLikedDao.java
@@ -1,7 +1,12 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Post;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JdbcLikedDao implements LikedDao{
@@ -38,5 +43,35 @@ public class JdbcLikedDao implements LikedDao{
         String sql = "SELECT COUNT(*) FROM likes WHERE post_id = ?";
         Integer likes = jdbcTemplate.queryForObject(sql, Integer.class, postId);
         return likes;
+    }
+
+    @Override
+    public List<Post> getListOfLikedPostsByAccountId(int accountId){
+        List<Post> favoritePosts = new ArrayList<>();
+        String sql = "SELECT posts.post_id, posts.account_id, posts.img, posts.caption, posts.post_date, posts.privated FROM likes\n" +
+                "INNER JOIN posts ON likes.post_id = posts.post_id\n" +
+                "INNER JOIN accounts ON likes.account_id = accounts.account_id\n" +
+                "WHERE accounts.account_id = 1\n" +
+                "ORDER BY posts.post_date DESC;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, accountId);
+
+        while (results.next()) {
+            Post post = MapRowToPost(results);
+            favoritePosts.add(post);
+        }
+        return favoritePosts;
+    }
+
+    private Post MapRowToPost(SqlRowSet results) {
+        Post post = new Post();
+
+        post.setPostId(results.getInt("post_id"));
+        post.setAccountId(results.getInt("account_id"));
+        post.setImg(results.getString("img"));
+        post.setPostDate(results.getTimestamp("post_date").toLocalDateTime());
+        post.setPrivated(results.getBoolean("privated"));
+        post.setCaption(results.getString("caption"));
+
+        return post;
     }
 }
--- java/src/main/java/com/techelevator/dao/LikedDao.java
+++ java/src/main/java/com/techelevator/dao/LikedDao.java
@@ -1,5 +1,9 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Post;
+
+import java.util.List;
+
 public interface LikedDao {
 
     boolean isLiked(int postId, int accountId);
@@ -9,4 +13,7 @@ public interface LikedDao {
     void unlike(int postId, int accountId);
 
     int getNumLikes(int postId);
+
+    List<Post> getListOfLikedPostsByAccountId(int accountId);
+
 }
